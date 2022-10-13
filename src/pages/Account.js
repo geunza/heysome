9a77c3d4721aac0792ab4d8165b043151c0b0bab
geunyoung-sbs <safa940812@naver.com>
2022-10-14 00:59:20 +0900

    221014

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,13 +1,40 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useState } from "react";
+import { useParams, Link } from "react-router-dom";
 import AccountTop from "components/AccountTop";
+import SignUp from "components/SignUp";
+import SignIn from "components/SignIn";
+import FindPassword from "components/FindPassword";
+import FindAccount from "components/FindAccount";
+import styles from "scss/Account.module.scss";
 const Account = () => {
   const { id } = useParams();
-  console.log(id);
+  // const [param, setParam] = useState("");
+  // useEffect(()=>{
+  //   setParam(id);
+  // }, id)
   return (
     <>
-      <AccountTop />
-      <div>Account</div>
+      <AccountTop styles={styles} />
+      <div id={styles.AccountBottom} style={{ position: "relative" }}>
+        <div
+          style={{
+            position: "absolute",
+            right: 0,
+            top: 0,
+            display: "flex",
+            gap: "15px",
+          }}
+        >
+          <Link to="/account/signUp">signUp</Link>
+          <Link to="/account/signIn">signIn</Link>
+          <Link to="/account/findPassword">findPassword</Link>
+          <Link to="/account/findAccount">findAccount</Link>
+        </div>
+        {id == "signIn" && <SignIn styles={styles} />}
+        {id == "signUp" && <SignUp styles={styles} />}
+        {id == "findPassword" && <FindPassword styles={styles} />}
+        {id == "findAccount" && <FindAccount styles={styles} />}
+      </div>
     </>
   );
 };
